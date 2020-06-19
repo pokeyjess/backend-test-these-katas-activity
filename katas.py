1d9d16e2904126909681915a3284bc5eddd080b6
Jessica Benson <pokeyjess72@gmail.com>
2020-06-19 14:46:25 -0400

    completes assessment

--- katas.py
+++ katas.py
@@ -1,10 +1,8 @@
 def add(x, y):
-    """Return the result of adding x to y."""
     return x + y
 
 
 def multiply(x, y):
-    """Return the result of multiplying x by y."""
     result = 0
 
     for _ in range(abs(y)):
@@ -14,7 +12,6 @@ def multiply(x, y):
 
 
 def power(x, n):
-    """Return the result of taking x to the nth power."""
     result = 1
 
     for _ in range(n):
@@ -24,26 +21,21 @@ def power(x, n):
 
 
 def factorial(n):
-    """Return the result of calculating the factorial of n."""
     result = 1
 
     for i in range(1, n + 1):
         result = multiply(result, i)
-
-    return result
+    if n < 0:
+        print("Cannot compute a negative number")
+    else:
+        return result
 
 
 def fibonacci(n):
-    """Return the nth number in the fibonacci sequence (starting at 0)."""
-    if n < 3:
-        return n - 1
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a+b
+    if n < 0:
+        print("Cannot compute a negative number")
     else:
-        second_to_last = 0
-        last = 1
-        result = 1
-
-        for _ in range(2, n):
-            result = second_to_last + last
-            second_to_last, last = last, result
-
-        return result
+        return a
--- tests/test_katas.py
+++ tests/test_katas.py
@@ -4,19 +4,29 @@ import katas
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(10, 5), 15)
+        self.assertEqual(katas.add(-1, 1), 0)
+        self.assertEqual(katas.add(-1, -1), -2)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(3, 5), 15)
+        self.assertEqual(katas.multiply(5, 10), 50)
+        self.assertEqual(katas.multiply(-1, -1), 1)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(3, 5), 243)
+        self.assertEqual(katas.power(5, 10), 9765625)
+        self.assertEqual(katas.power(-1, -1), 1)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        self.assertEqual(katas.factorial(5), 120)
+        self.assertEqual(katas.factorial(3), 6)
+        self.assertEqual(katas.factorial(-1), None)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        self.assertEqual(katas.fibonacci(5), 5)
+        self.assertEqual(katas.fibonacci(3), 2)
+        self.assertEqual(katas.fibonacci(-1), None)
 
 
 if __name__ == '__main__':
